297f20e40edacb2cd48d9b819dd309f411fd618a
Paulo Henrique Gonçalves Rezende <rezendepauloh@gmail.com>
2026-03-17 14:07:00 -0400

    Retirando dados sensíveis

---
+++ manual_entries_TEMPLATE.py
@@ -0,0 +1,13 @@
+def get_manual_entries():
+    return [
+        {
+            "Cidade": "Cidade Exemplo",
+            "Tipo": "Setor Interno",
+            "Setor": "Nome do Setor",
+            "Sigla": "SIGLA",
+            "Titular": "Fulano de Tal",
+            "Unidade (Prédio)": "Prédio Principal",
+            "Telefone": "3333-3333",
+            "URL": ""
+        }
+    ]
